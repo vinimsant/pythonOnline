8236c8448cac659698d2e9dcd2cbda1dc08fed0f
vinimsant <vini.v2@hotmail.com>
2022-12-23 17:17:55 +0000

    salvar alterações

--- python/hello word.py
+++ python/hello word.py
@@ -1,3 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout
 
+class Calculador(QMainWindow()):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle('Calculadora')
+        self.setFixedSize(400, 400)
