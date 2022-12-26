e3e4fd15b4d5e3d2f620b6bb4689a28c714274e7
vinimsant <vini.v2@hotmail.com>
2022-12-26 16:24:44 +0000

    php

---
+++ html/cadastro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam aliquam facere cum laudantium blanditiis hic non fugit ipsum sit ipsam, dicta accusantium, iure optio sint, temporibus porro. Quos, est ducimus.
+    </p>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam qui architecto nulla modi reiciendis, aliquam eius est illo, nihil repellendus nisi pariatur? Magni placeat, dolores maiores deserunt nesciunt nisi fugit!
+    </p>
+</body>
+</html>
